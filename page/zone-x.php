b9128d863f103c510bc2745066f5f38494f9e071
azizyh1 <128148044+azizyh1@users.noreply.github.com>
2023-04-06 00:14:28 +0100

    Update zone-x.php

--- page/zone-x.php
+++ page/zone-x.php
@@ -35,71 +35,26 @@
 
 	<div id="Container">
         <p>Quantité de Stock</p>
-        <form action="../affichage.php" method="POST"></form>
-        <table id="quantité">
-            <tr>
-                <td><?php
-			// Établir une connexion avec la base de données
-			$conn = mysqli_connect("localhost", "root", "", "datasave");
-
-			// Vérifier la connexion
-			if (!$conn) {
-			    die("Erreur de connexion : " . mysqli_connect_error());
-			}
-
-			// Effectuer une requête SQL pour récupérer le nombre de données restantes dans la table
-			$sql = "SELECT COUNT(*) AS total FROM stockmp ";
-			$result = mysqli_query($conn, $sql);
-
-			// Vérifier si la requête a réussi
-			if (!$result) {
-			    die("Erreur de requête : " . mysqli_error($conn));
-			}
-
-			// Récupérer le nombre de données restantes dans la table
-			$row = mysqli_fetch_assoc($result);
-			$restantes = $row["total"];
-
-			// Afficher le nombre de données restantes dans la table
-			echo $restantes;
-			?></td>
-            </tr>
+		
+        
+		<table>
+			<tr>
+				<th id="total" name="total" ></th>
+			</tr>
+    <?php include '../affichage.php';?>
         </table>
 	
 </div>
 
-	<?php
-        $conn = mysqli_connect('127.0.0.1', 'root', '', 'datasave');
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        $query = "SELECT * FROM stockmp";
-        $result = mysqli_query($conn, $query);
-        if (!$result) {
-            echo 'erreur' . mysqli_error($conn);
-        } else {
-    ?>
-    <div class="zone">
-        <table id="mm">
-            <tr>
-                <th>Palette</th>
-                <th>Emplacement</th>
-                <th>code a barre</th>
-                <th>Date et heure dentree</th>
-            </tr>
-            <?php while ($row = mysqli_fetch_array($result)) { ?>
-                <tr>
-                    <td><?php echo $row['codepalette'] ?></td>
-                    <td><?php echo $row['emplacement'] ?></td>
-                    <td><?php echo $row['codeabarre'] ?></td>
-                    <td><?php echo $row['dateheure'] ?></td>
-                </tr>
-            <?php } ?>
-        </table>
-    </div>
-    <?php
-        }
-        mysqli_close($conn);
-    ?>
+<table id="mm">
+    <tr>
+        <th id="Palette" name="codepalette" >Palette</th>
+        <th id="emplacement" name="emplacement" >Emplacement</th>
+        <th id="codeabarre" name="codeabarre">code a barre</th>
+        <th id="dateheure" name="dateheure" >Date et heure dentree</th>
+    </tr>
+    <?php include '../table.php';?>
+</table>
+</body>
 
 		</html>
